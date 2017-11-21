88d8a85737a61872d90ae00121f04f0b160681e8
antonc27 <antonc27@ail.ru>
2017-11-21 16:49:28 +0100

    - fix path to data files in tests

--- assn-2-six-degrees/imdb-test.cc
+++ assn-2-six-degrees/imdb-test.cc
@@ -200,7 +200,7 @@ static void queryForActors(const imdb& db)
 
 int main(int argc, char **argv)
 {
-  imdb db(determinePathToData());
+  imdb db(determinePathToData("./assn-2-six-degrees-data/little-endian"));
   if (!db.good()) { cerr << "Data directory not found!  Aborting..." << endl; return 1; }
   queryForActors(db);
   return 0;
