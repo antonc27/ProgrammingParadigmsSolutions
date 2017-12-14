b415c8355766e3db97e3b91a64d4bab17467a950
antonc27 <antonc27@ail.ru>
2017-12-14 21:18:21 +0100

    - sortedset tests

--- sect-2/sorted_set_test.c
+++ sect-2/sorted_set_test.c
@@ -1,5 +1,28 @@
 #include "sorted_set.h"
+#include <stdio.h>
+#include <assert.h>
+
+int cmpint(const void *a, const void *b) {
+  return *(int *)a - *(int *)b;
+}
 
 int main() {
-	return 0;
+  //printf("Hi\n");
+  //void SetNew(sortedset *set, int elemSize, int (*cmpfn)(const void *, const void *))
+  int ints[4] = {2, 7, 3, 6};
+  
+  sortedset set;
+  SetNew(&set, sizeof(int), cmpint);
+
+  for (int i = 0; i < 4; i++) {
+    SetAdd(&set, ints + i);
+  }
+
+  int notFound = 10;
+  assert(SetSearch(&set, &notFound) == NULL);
+
+  int found = 6;
+  assert(SetSearch(&set, &found) != NULL);
+  
+  return 0;
 }
