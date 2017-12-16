c8640982527bc804692917462563d5b312f866e0
antonc27 <antonc27@mail.ru>
2017-12-16 21:56:42 +0000

    - finish SetNew

--- sect-2/sorted_set.c
+++ sect-2/sorted_set.c
@@ -1,5 +1,6 @@
 #include "sorted_set.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
 static const int kInitialCapacity = 4;
@@ -9,6 +10,16 @@ void SetNew(sortedset *set, int elemSize, int (*cmpfn)(const void *, const void 
   set->index = 0;
   set->elemSize = elemSize;
   set->cmpFn = cmpfn;
+
+  size_t nodeSize = elemSize + 2 * sizeof(int);
+  set->base = malloc(kInitialCapacity * nodeSize);
+  assert(set->base != NULL);
+  
+  set->allocLen = malloc(sizeof(int));
+  *set->allocLen = kInitialCapacity;
+
+  set->logLen = malloc(sizeof(int));
+  *set->logLen = 0;
 }
 
 void *SetSearch(sortedset *set, const void *elemPtr) {
