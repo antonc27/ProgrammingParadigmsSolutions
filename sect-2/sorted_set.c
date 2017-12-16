ffa0c4ed6c7d5645564977cfa00ff2b26742b730
antonc27 <antonc27@ail.ru>
2017-12-16 20:17:10 +0100

    - SetNew, WIP

--- sect-2/sorted_set.c
+++ sect-2/sorted_set.c
@@ -1,8 +1,14 @@
 #include "sorted_set.h"
+#include <stdio.h>
+#include <assert.h>
 
 static const int kInitialCapacity = 4;
 void SetNew(sortedset *set, int elemSize, int (*cmpfn)(const void *, const void *)) {
-
+  assert(elemSize > 0);
+  assert(cmpfn != NULL);
+  set->index = 0;
+  set->elemSize = elemSize;
+  set->cmpFn = cmpfn;
 }
 
 void *SetSearch(sortedset *set, const void *elemPtr) {
