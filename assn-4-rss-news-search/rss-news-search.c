5aaf1508cb784278a2b6875e5f679f3657c04b15
antonc27 <antonc27@mail.ru>
2018-03-27 23:49:41 +0200

    - STILL CRASHING ON FREEEEEEE, WIP

--- assn-4-rss-news-search/rss-news-search.c
+++ assn-4-rss-news-search/rss-news-search.c
@@ -72,9 +72,9 @@ static void FillStopWords(const char* stopWordsFileName, hashset *hashset)
   file = fopen(stopWordsFileName, "r");
   STNew(&st, file, kWhiteSpaceCharacters, true);
   while (STNextToken(&st, word, sizeof(word))) {
-    char *dummy = word;
-    char *s = (char *)strdup(dummy);
-    printf("%s\n", s);
+    //    char *dummy = word;
+    char *s = strdup(word);
+    printf("%s 0x%08lx\n", s, (long unsigned int)s);
     HashSetEnter(hashset, s);
     //free(s);
   }
