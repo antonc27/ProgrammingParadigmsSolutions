c30d293a3b5009d3ad54fbcfd85c7108fb5b688f
antonc27 <antonc27@mail.ru>
2018-04-11 23:50:33 +0200

    - sect assn 3 problem 3,4 solutions

--- sect-3/problem-3-4/multitable-test.c
+++ sect-3/problem-3-4/multitable-test.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "multitable.h"
 
+static const signed long kHashMultiplier = -1664117991L;
+static int StringHash(const char *s, int numBuckets)
+{
+    int i;
+    unsigned long hashcode = 0;
+    for (i = 0; i < strlen(s); i++) {
+        hashcode = hashcode * kHashMultiplier + tolower(s[i]);
+    }
+    return hashcode % numBuckets;
+}
+
 static int ZipCodeHash(const void *keyAddr, int numBuckets) {
-    return 0;
+    char *zipcode = (char *)keyAddr;
+    return StringHash(zipcode, numBuckets);
 }
 
 static int ZipCodeCompare(const void *keyAddr1, const void *keyAddr2) {
-    return 0;
+    return strcmp((char *)keyAddr1, (char *)keyAddr2);
 }
 
 static void InRangePrint(void *keyAddr, void *valueAddr, void *auxData) {
-    char *zipcode;
-    char *city;
-    char *low;
-    char *high;
+    char *zipcode = (char *)keyAddr;
+    char *city = *(char **)valueAddr;
+    char *low = *((char **)auxData);
+    char *high = *((char **)auxData + 1);
     
     if ((strcmp(zipcode, low) >= 0) && (strcmp(zipcode, high) <= 0)) {
         printf("%5s: %s\n", zipcode, city);
@@ -33,11 +46,11 @@ int main(int argc, const char * argv[]) {
     
     char code[6] = {"08077"};
     code[5] = '\0';
-    char *cities[3] = {"Cinnaminson, NJ", "Cinnaminson, NJ", "Palmyra, NJ"};
+    char *cities[3] = {"Cinnaminson, NJ", "Riverton, NJ", "Palmyra, NJ"};
     
-    MultiTableEnter(&zipCodeDatabase, &code, cities[0]);
-    MultiTableEnter(&zipCodeDatabase, &code, cities[1]);
-    MultiTableEnter(&zipCodeDatabase, &code, cities[2]);
+    MultiTableEnter(&zipCodeDatabase, &code, &cities[0]);
+    MultiTableEnter(&zipCodeDatabase, &code, &cities[1]);
+    MultiTableEnter(&zipCodeDatabase, &code, &cities[2]);
     
     ListRecordsInRange(&zipCodeDatabase, "08076", "08078");
     
--- sect-3/problem-3-4/multitable.c
+++ sect-3/problem-3-4/multitable.c
@@ -1,15 +1,49 @@
 #include "multitable.h"
+#include <stdlib.h>
+#include <string.h>
 
 void MultiTableNew(multitable *mt, int keySizeInBytes, int valueSizeInBytes, int numBuckets,
                    MultiTableHashFunction hash,
                    MultiTableCompareFunction compare) {
-    
+    mt->keySize = keySizeInBytes;
+    mt->valueSize = valueSizeInBytes;
+    HashSetNew(&mt->mappings, keySizeInBytes + sizeof(vector), numBuckets, hash, compare, NULL);
 }
 
 void MultiTableEnter(multitable *mt, const void *keyAddr, const void *valueAddr) {
-    
+    void *existing = HashSetLookup(&mt->mappings, keyAddr);
+    if (existing == NULL) {
+        void *newKeyValue = malloc(mt->keySize + sizeof(vector));
+        memcpy(newKeyValue, keyAddr, mt->keySize);
+        vector *value = (vector *)((char *)newKeyValue + mt->keySize);
+        VectorNew(value, mt->valueSize, NULL, 1);
+        VectorAppend(value, valueAddr);
+        HashSetEnter(&mt->mappings, newKeyValue);
+        free(newKeyValue);
+    } else {
+        vector *value = (vector *)((char *)existing + mt->keySize);
+        VectorAppend(value, valueAddr);
+    }
+}
+
+typedef struct {
+    multitable *mt;
+    MultiTableMapFunction map;
+    void *auxData;
+} helpAuxData;
+
+void hashSetMapFunction(void *elemAddr, void *setAuxData) {
+    helpAuxData *had = (helpAuxData *)setAuxData;
+    vector *value = (vector *)((char *)elemAddr + had->mt->keySize);
+    for (int i = 0; i < VectorLength(value); i++) {
+        had->map(elemAddr, VectorNth(value, i), had->auxData);
+    }
 }
 
 void MultiTableMap(multitable *mt, MultiTableMapFunction map, void *auxData) {
-    
+    helpAuxData had;
+    had.mt = mt;
+    had.map = map;
+    had.auxData = auxData;
+    HashSetMap(&mt->mappings, hashSetMapFunction, &had);
 }
