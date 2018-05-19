2283732eb4f33d65970cbdcc1d5370bb7c59c603
antonc27 <antonc27@mail.ru>
2018-05-19 23:57:27 +0200

    testing unsigned shorts overflows

--- assn-5-raw-memory/problem-1/binaries.c
+++ assn-5-raw-memory/problem-1/binaries.c
@@ -26,6 +26,10 @@ void print_short(short x) {
   print_bytes((bytes_pointer) &x, sizeof(short));
 }
 
+void print_u_short(unsigned short x) {
+  print_bytes((bytes_pointer) &x, sizeof(unsigned short));
+}
+
 bool tadd_ok(short x, short y) {
   short sum = x+y;
   if (x > 0 && y > 0 && sum <= 0) {
@@ -36,10 +40,24 @@ bool tadd_ok(short x, short y) {
   return true;
 }
 
+bool uadd_ok(unsigned short x, unsigned short y) {
+  if (x == 0 || y == 0) {
+    return true;
+  }
+  return ((unsigned short)(x+y) > x);
+}
+
 void print_short_info(char *desc, short s) {
   printf("%s dec %hd\n", desc, s);
   printf("%s hex", desc);
   print_short(s);
+  printf("\n");
+}
+
+void print_u_short_info(char *desc, unsigned short us) {
+  printf("%s dec %hu\n", desc, us);
+  printf("%s hex", desc);
+  print_u_short(us);
   printf("\n");
 }
 
@@ -52,9 +70,21 @@ void test_short_overflow(short s1, short s2) {
   printf("--- ---\n");
 }
 
+void test_u_short_overflow(unsigned short us1, unsigned short us2) {
+  printf("--- Testing unsigned shorts us1(%hu)+us2(%hu) for overflow ---\n", us1, us2);
+  print_u_short_info("us1", us1);
+  print_u_short_info("us2", us2);
+  print_u_short_info("us1+us2", us1+us2);
+  printf(uadd_ok(us1, us2) ? "Not overflows\n" : "Overflows\n");
+  printf("--- ---\n");
+}
+
 void OverflowTest() {
   test_short_overflow(20000, 10000);
   test_short_overflow((short)((1<<15)-1), 1);
+
+  test_u_short_overflow(20000, 10000);
+  test_u_short_overflow((unsigned short)((1<<16)-1), 1);
 }
 
 int main() {
